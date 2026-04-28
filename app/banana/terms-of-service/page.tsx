6867971f446693d3ac9ce3cb5894ebc86167fe3e
karlozrilic <karlozrilic@gmail.com>
2026-04-28 20:51:41 +0200

    Small update

---
+++ app/banana/terms-of-service/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useEffect, useState } from 'react';
+import LoadingScreen from '@/app/sections/loading';
+
+export default function Banana() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(false);
+    });
+
+    return (
+        <>
+            {loading && <LoadingScreen />}
+            <main className='flex min-h-screen flex-col items-center p-24'>
+                <h1 className='text-4xl font-bold text-center mb-10'>Terms of Service</h1>
+                <p>Work in progress</p>
+            </main>
+        </>
+    );
+}
