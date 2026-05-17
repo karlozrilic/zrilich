b363b7ad65045be1bba0611a7afad671eb800a60
karlozrilic <karlozrilic@gmail.com>
2026-05-17 20:18:33 +0200

    fixed image

--- app/sections/project_page.tsx
+++ app/sections/project_page.tsx
@@ -36,7 +36,7 @@ export default function ProjectPage() {
             <div className='relative'>
                 <Image
                     className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] mb-5'
-                    src='/images/banana/banana.svg'
+                    src='/images/banana/banana.png'
                     alt='Banana Logo'
                     width={180}
                     height={37}
